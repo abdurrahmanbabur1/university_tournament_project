5ad1faeb5147d4588c68c3231e51c3fa2c384075
Abdurrahman Babur <abdurrahmanbabur90@gmail.com>
2026-02-12 15:47:18 +0300

    emoji eklendi
    emojiler müsabaka takvimine eklendi.

--- Models/HomeViewModel.cs
+++ Models/HomeViewModel.cs
@@ -5,7 +5,9 @@ namespace UniversityTournamentPro.Models
     public class HomeViewModel
     {
         public List<Match> Matches { get; set; }
+        public List<Tournament> Tournaments { get; set; } // Takvimde turnuva başlangıçlarını da göstermek için
         public List<Announcement> Announcements { get; set; }
- public int PendingUserCount { get; set; } // Onay bekleyen öğrenci sayısı için
+        public List<Branch> Branches { get; set; } // Branş emojileri için
+        public int PendingUserCount { get; set; } // Onay bekleyen öğrenci sayısı için
     }
 }
